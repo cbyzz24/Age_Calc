5886ca47421e38385e6ec09ce1126dc5f38bccae
Christopher Belcher <cpbelcher1@gmail.com>
2024-10-04 19:08:19 -0600

    Current year auto populate updates

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const birthYear = document.getElementById("birthYear");
 const calculateButton = document.getElementById("calculateButton");
 const result = document.getElementById("result");
 
+function updateYear(){
+    const thisYear = new Date().getFullYear();
+    const currentYear = document.getElementById("currentYear").textContent = thisYear;
+}
 
+updateYear();
 
 function calculateAge(){
     result.textContent = currentYear.value - birthYear.value;
